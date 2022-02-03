40e9d40ac855296722b7a19fd7c2dd166afd98ce
xen0vas <xenofonv@gmail.com>
2022-02-03 17:11:12 +0200

    Update rev_shell.c

--- rev_shell.c
+++ rev_shell.c
@@ -1,35 +1,8 @@
 /*
-# Title: Windows Reverse TCP shellcode (192.168.201.11:4444) (330 Bytes)
-# Date: 09.12.2021
-# Author: Xenofon Vassilakopoulos
-# Tested on: Windows/x64 - 10.0.19043 N/A Build 19043 
-*/
-
-/*
-
-MIT License
-
-Copyright (c) 2021 Xenofon Vassilakopoulos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-
+;; Title: Windows Reverse TCP shellcode (192.168.201.11:4444) (330 Bytes)
+;; Date: 09.12.2021
+;; Author: Xenofon Vassilakopoulos
+;; Tested on: Windows/x64 - 10.0.19043 N/A Build 19043 
 
 [BITS 32]
 
